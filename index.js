715c486a2c1dd34306ab145f8c50d126ec862873
mdujjal67 <mdujjal.mm67@gmail.com>
2024-06-01 21:55:47 +0600

    added jwt functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -31,9 +32,38 @@ async function run() {
     const cartCollection = client.db('bistroDB').collection('carts');
 
 
+     // -------------jwt related api--------------
+     app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' }); //token generate
+
+      // the commented code would work only localhost but bellow codes would work with production and localhost also.
+      res.send({ token })
+    });
+
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({ message: 'forbidden access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({ message: 'forbidden  access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
     // user related api
 
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken, async(req, res) => {
+      console.log(req.headers)
       const result = await userCollection.find().toArray();
       res.send(result);
     });
