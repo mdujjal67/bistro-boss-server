7f00a33e93e514c88f7b0df428e054a9373c4a63
mdujjal67 <mdujjal.mm67@gmail.com>
2024-06-02 20:14:15 +0600

    added admin verify api

--- index.js
+++ index.js
@@ -47,22 +47,36 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log("inside verify token", req.headers.authorization);
       if(!req.headers.authorization){
-        return res.status(401).send({ message: 'forbidden access' })
+        return res.status(401).send({ message: 'unauthorized access' })
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err){
-          return res.status(401).send({ message: 'forbidden  access' })
+          return res.status(401).send({ message: 'unauthorized  access' })
         }
         req.decoded = decoded;
         next();
       })
+    };
+
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({ message: 'forbidden access'})
+      };
+      next()
     }
+
 
 
     // user related api
 
-    app.get('/users', verifyToken, async(req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async(req, res) => {
       console.log(req.headers)
       const result = await userCollection.find().toArray();
       res.send(result);
@@ -75,6 +89,23 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
+
+
+    // check if user isAdmin
+    app.get('/user/admin/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({ message: 'forbidden access'})
+      }
+
+      const query = { email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin })
+    })
 
 
     app.patch('/users/admin/:id', async(req, res) => {
